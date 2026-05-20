e8ed5a258b02138411eaef6f3ef157f5b6921d8e
Ben Cao <benc@swivel.ai>
2026-05-20 10:31:27 -0400

    fixup: pin real sha256 for v0.7.6 (816992d3…)

--- Formula/pilotdesk-flow.rb
+++ Formula/pilotdesk-flow.rb
@@ -10,7 +10,7 @@ class PilotdeskFlow < Formula
   desc "Pilotdesk flow CLI for isolated dev environments"
   homepage "https://github.com/Pilotdesk/pilotdesk-flow-cli"
   url      "https://storage.googleapis.com/pilotdesk-flow-releases-swivel-labs/v0.7.6/pilotdesk-flow-v0.7.6.tar.gz"
-  sha256   "REPLACE_WITH_SHA256_FROM_FIRST_RELEASE"
+  sha256   "816992d3e392ce2accd7c89018b0ba2fcc57c4c853503145da1c916a15bea208"
   version  "0.7.6"
   license  "MIT"
 
